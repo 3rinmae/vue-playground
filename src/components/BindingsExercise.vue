4051af732ca4f699c4f6142b0db9a8d2d4769efe
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-10-23 14:42:03 -0600

    looping done

--- src/components/BindingsExercise.vue
+++ src/components/BindingsExercise.vue
@@ -22,9 +22,9 @@
             value changes.
           </p>
           <!-- add a v-model to this input that will reference "myName" within the state object-->
-          <input />
+          <input v-model="state.myName" />
           <!-- reference "myName" from the state -->
-          <p>{{}}</p>
+          <p> Hello {{ state.myName }}</p>
         </div>
         <div class="border p-1">
           <h5>Class Binding</h5>
@@ -37,7 +37,7 @@
           </p>
           <div class="red">
             <!-- add a class binding attribute to the p tag that references the state isActive value -->
-            <p>This text will turn green on active</p>
+            <p :class="{ active: state.isActive }">This text will turn green on active</p>
           </div>
         </div>
       </div>
@@ -54,9 +54,10 @@ export default {
     // NOTE typically state will be abstracted to a global AppState
     const state = reactive({
       //create a property called myName and set its value to "type your name here"
-      message: "Super secret message here!",
+      message: "Super secret message here! Testing new message",
+      myName: "type your name here",
       //change isActive to true
-      isActive: false,
+      isActive: true,
     });
     return { state };
   },
